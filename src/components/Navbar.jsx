11aea073536011199fd2309c0e944676f2e0b7bc
Anmol-123 <vanmol.1998@gmail.com>
2021-06-12 13:09:07 +0530

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Navbar = () => {
     return (
         <div className="text-light text-center p-4 mt-5 " >
-            <h2 style={{ cursor: 'pointer', fontSize: '3rem' }} className="font-weight-bold ">Covifier5 <i class="fa fa-heartbeat" aria-hidden="true"></i>
+            <h2 style={{ cursor: 'pointer', fontSize: '3rem' }} className="font-weight-bold ">Covifier <i class="fa fa-heartbeat" aria-hidden="true"></i>
             </h2>
         </div>
     )
